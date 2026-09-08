d950a9792d5ce3384f2d5998da8307f8c596f5f5
David_G4 <davidfablyn@gmail.com>
2026-09-08 18:08:31 +0100

    i made some recent changes

---
+++ strict.mode.js
@@ -0,0 +1,97 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('i can drive');
+
+const interface = 'audio';
+const private = 3444;
+
+function logger() {
+    console.log('my name is David');
+} 
+
+logger();
+logger();
+logger();
+
+
+function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const applesJuice = fruitProcessor(5, 0);
+console.log(applesJuice);
+// console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('34');
+
+// function declaration
+function calcAge1(birthYear) {
+    // const age = 2037 - birthYear;
+    return 2037 -  birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+
+// functions expression
+const calcAge2 = function (birthYear) {
+    return 2037 -  birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+function greetStudent(name) {
+    console.log("Welcome David");
+}
+
+greetStudent();
+greetStudent();
+greetStudent();
+
+
+
+let checkAge = function checkAge(age){
+    if (age >= 18){
+        console.log('you are eligible');
+    }else{
+        console.log('you are not eligible');
+    }
+}
+
+checkAge(20);
+checkAge(15);
+checkAge(18);
+
+    function calculateTotal(price, quantity) {
+        let total = price * quantity;
+
+        return total;
+    }
+    let amount = calculateTotal(5000, 3);
+    let deliveryFee = 4000
+    let finalAmount = amount * deliveryFee;
+
+console.log(finalAmount);
+
+
+
+function applyDiscount(price, discountPercentage){
+    let discount = price * discountPercentage / 100;
+    let finalPrice = price - discount
+
+    return finalPrice;
+}
+console.log(applyDiscount(10000, 10));
+
